36ced02ec8a2d00e5a81d7ee5af1f128d61a472b
DimitrijeVeljkovic <dimitrijeveljkovic2@gmail.com>
2026-08-11 13:40:32 +0200

    Add router and views

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,38 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'root',
+      redirect: '/home',
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: () => import('../views/HomeView.vue'),
+    },
+    {
+      path: '/store',
+      name: 'store',
+      component: () => import('../views/StoreView.vue'),
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('../views/ContactView.vue'),
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: () => import('../views/CartView.vue'),
+    },
+  ],
 })
 
 export default router
---
+++ src/views/CartView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Cart</h1>
+</template>
+
+<style scoped></style>
