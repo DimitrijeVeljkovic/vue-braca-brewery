38afb867bc1702ef4cdcf2908a1b28993525e349
DimitrijeVeljkovic <dimitrijeveljkovic2@gmail.com>
2026-08-11 22:45:10 +0200

    Add hero section

---
+++ src/components/Hero.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="hero">
+    <div class="hero-text">
+        <h1>
+            <span>Lorem ipsum dolor sit amet</span>
+            <br>
+            <span class="highlight">consectetur adipisicing elit</span>
+        </h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quod, quia, 
+            voluptatum, voluptates, quibusdam, quidem, quos, quae, quibusdam, quibusdam, quibusdam.
+        </p>
+        <div class="hero-buttons">
+            <router-link to="/store" class="button button-store">VISIT STORE ></router-link>
+            <router-link to="/about" class="button button-about">Learn More</router-link>
+        </div>
+    </div>
+    <img src="https://insights.ehl.edu/hubfs/1440/1440x960-beer-types.jpg" alt="Hero Image" />
+  </div>
+</template>
+
+<style scoped>
+.hero {
+    padding-left: 80px;
+    display: flex;
+    justify-content: space-between;
+    gap: 40px;
+
+    img {
+        height: 760px;
+        width: 50%;
+        object-fit: cover;
+    }
+
+    .hero-text {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        gap: 32px;
+
+        h1 {
+            font-size: 90px;
+
+            .highlight {
+                color: #E2FF00;
+            }
+        }
+
+        p {
+            font-size: 18px;
+            color: #A5A5B0;
+            width: 70%;
+            font-family: 'GeistRegular', sans-serif;
+        }
+
+        .hero-buttons {
+            display: flex;
+            gap: 20px;
+
+            .button {
+                border: 1px solid #E2FF00;
+                background-color: #E2FF00;
+                border-radius: 3px;
+                padding: 18px 32px;
+                text-decoration: none;
+                color: #0E0E12;
+                font-family: 'GeistMonoRegular', sans-serif;
+                font-size: 18px;
+            }
+
+            .button-about {
+                border-color: #1C1C24;
+                background-color: transparent;
+                color: #F8F8F9;
+            }
+        }
+    }
+}
+</style>
