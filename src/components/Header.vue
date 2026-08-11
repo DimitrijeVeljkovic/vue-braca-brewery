c062bc526165b19813b0011608757820a9fd6138
DimitrijeVeljkovic <dimitrijeveljkovic2@gmail.com>
2026-08-11 14:18:02 +0200

    Header changes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,34 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Header</h1>
+  <div class="nav-menu">
+    <div>
+      <router-link to="/about">ABOUT</router-link>
+      <router-link to="/contact">CONTACT</router-link>
+    </div>
+    <div>
+      <router-link to="/">HOME</router-link>
+    </div>
+    <div>
+      <router-link to="/store">STORE</router-link>
+      <router-link to="/cart"><i class="pi pi-shopping-cart"></i>0</router-link>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.nav-menu {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 30px;
+
+  a {
+    text-decoration: none;
+    color: #333;
+    font-weight: bold;
+    margin: 0 10px;
+    vertical-align: middle;
+  }
+}
+</style>
